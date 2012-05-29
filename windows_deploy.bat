416da0d2fd08c30dfef462b7292623da663bfeb0
Graeme Gott <graeme@gottcode.org>
2012-05-29 07:02:02 -0400

    Update deployment scripts.

--- windows_deploy.bat
+++ windows_deploy.bat
@@ -1,7 +1,34 @@
-MKDIR CuteMaze
-COPY release\CuteMaze.exe CuteMaze
-COPY %QTDIR%\bin\mingwm10.dll CuteMaze
-COPY %QTDIR%\bin\QtCore4.dll CuteMaze
-COPY %QTDIR%\bin\QtGui4.dll CuteMaze
-COPY %QTDIR%\bin\QtXml4.dll CuteMaze
-COPY %QTDIR%\bin\QtSvg4.dll CuteMaze
+@ECHO OFF
+
+SET APP=CuteMaze
+FOR /f %%i IN ('git rev-parse --short HEAD') DO SET VERSION=%%i
+
+ECHO Copying executable
+MKDIR %APP%
+TYPE COPYING | FIND "" /V > %APP%\COPYING.txt
+TYPE CREDITS | FIND "" /V > %APP%\CREDITS.txt
+COPY release\%APP%.exe %APP% >nul
+strip %APP%\%APP%.exe
+
+ECHO Copying translations
+SET TRANSLATIONS=%APP%\translations
+MKDIR %TRANSLATIONS%
+COPY translations\*.qm %TRANSLATIONS% >nul
+COPY %QTDIR%\translations\qt_*.qm %TRANSLATIONS% >nul
+
+ECHO Copying Qt libraries
+COPY %QTDIR%\bin\libgcc_s_dw2-1.dll %APP% >nul
+COPY %QTDIR%\bin\mingwm10.dll %APP% >nul
+COPY %QTDIR%\bin\QtCore4.dll %APP% >nul
+COPY %QTDIR%\bin\QtGui4.dll %APP% >nul
+COPY %QTDIR%\bin\QtXml4.dll %APP% >nul
+COPY %QTDIR%\bin\QtSvg4.dll %APP% >nul
+
+ECHO Creating compressed file
+CD %APP%
+7z a %APP%_%VERSION%.zip * >nul
+CD ..
+MOVE %APP%\%APP%_%VERSION%.zip . >nul
+
+ECHO Cleaning up
+RMDIR /S /Q %APP%
