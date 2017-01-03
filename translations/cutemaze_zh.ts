d0888077556b5a259397faf26486a04bf2b6b494
Graeme Gott <graeme@gottcode.org>
2017-01-03 05:21:26 -0500

    Sync translations with Transifex.

---
+++ translations/cutemaze_zh.ts
@@ -0,0 +1,425 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Sorry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to load previous game. A new game will be started.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 elapsed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 steps taken</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 of %2 targets remain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paused</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ControlButton</name>
+    <message>
+        <source>Shift</source>
+        <translation>Shift</translation>
+    </message>
+    <message>
+        <source>Ctrl</source>
+        <translation>Ctrl</translation>
+    </message>
+    <message>
+        <source>Meta</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alt</source>
+        <translation>Alt</translation>
+    </message>
+    <message>
+        <source>Super</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Command</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Control</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Option</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;系统语言&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewGameDialog</name>
+    <message>
+        <source>New Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hunt and Kill</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Kruskal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Prim</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recursive Backtracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack 4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack 5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Algorithm:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Targets:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QLabel</name>
+    <message>
+        <source>Hunt and Kill</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Kruskal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Prim</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recursive Backtracker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack 4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stack 5</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Scores</name>
+    <message>
+        <source>CuteMaze Scores</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Steps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Algorithm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Congratulations!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Your score has made the top ten.
+Please enter your name:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show where you&apos;ve been</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show number of steps taken</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show elapsed time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Smooth movement</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Controls</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Move Up:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Move Down:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Move Left:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Move Right:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Toggle Flag:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Themes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select Theme File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sorry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to create data folder.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to copy theme file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove the selected theme?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to remove the selected theme.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>CuteMaze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>P</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>H</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl++</source>
+        <translation>Ctrl键加+</translation>
+    </message>
+    <message>
+        <source>Ctrl+-</source>
+        <translation>Ctrl键加-</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Hint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>High &amp;Scores</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom &amp;In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Zoom &amp;Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Preferences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A top-down maze game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2007-%1 Graeme Gott</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Icons are from the &lt;a href=%1&gt;Oxygen&lt;/a&gt; theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Used under the &lt;a href=%1&gt;LGPL 3&lt;/a&gt; license</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
