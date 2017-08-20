fc87ecbf10248e5d060b7723d875c88a15cc2559
Graeme Gott <graeme@gottcode.org>
2017-08-20 08:09:40 -0400

    Sync translations with Transifex.

---
+++ translations/cutemaze_el.ts
@@ -0,0 +1,425 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="el">
+<context>
+    <name>Board</name>
+    <message>
+        <source>Sorry</source>
+        <translation>Συγνώμη</translation>
+    </message>
+    <message>
+        <source>Unable to load previous game. A new game will be started.</source>
+        <translation>Αδυναμία φόρτωσης προηγούμενου παιχνιδιού. Θ&apos; αρχίσει νέο παιχνίδι.</translation>
+    </message>
+    <message>
+        <source>%1 elapsed</source>
+        <translation>%1 παρήλθε</translation>
+    </message>
+    <message>
+        <source>%1 steps taken</source>
+        <translation>%1 βήματα έγιναν</translation>
+    </message>
+    <message>
+        <source>%1 of %2 targets remain</source>
+        <translation>%1 of %2 στόχοι παραμένουν</translation>
+    </message>
+    <message>
+        <source>Success</source>
+        <translation>Επιτυχία</translation>
+    </message>
+    <message>
+        <source>Paused</source>
+        <translation>Παύση</translation>
+    </message>
+</context>
+<context>
+    <name>ControlButton</name>
+    <message>
+        <source>Shift</source>
+        <translation>Shift</translation>
+    </message>
+    <message>
+        <source>Ctrl</source>
+        <translation>Ctrl</translation>
+    </message>
+    <message>
+        <source>Meta</source>
+        <translation>Meta</translation>
+    </message>
+    <message>
+        <source>Alt</source>
+        <translation>Alt</translation>
+    </message>
+    <message>
+        <source>Super</source>
+        <translation>Super</translation>
+    </message>
+    <message>
+        <source>Windows</source>
+        <translation>Windows</translation>
+    </message>
+    <message>
+        <source>Command</source>
+        <translation>Command</translation>
+    </message>
+    <message>
+        <source>Control</source>
+        <translation>Control</translation>
+    </message>
+    <message>
+        <source>Option</source>
+        <translation>Επιλογή</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <source>Select application language:</source>
+        <translation>Επιλογή γλώσσας εφαρμογής</translation>
+    </message>
+    <message>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;System Language&gt;</translation>
+    </message>
+    <message>
+        <source>Note</source>
+        <translation>Σημείωση</translation>
+    </message>
+    <message>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Παρακαλώ επανεκκινήστε το πρόγραμμα, για να εφαρμοστεί η αλλαγή γλώσσας. </translation>
+    </message>
+</context>
+<context>
+    <name>NewGameDialog</name>
+    <message>
+        <source>New Game</source>
+        <translation>Νέο παιχνίδι</translation>
+    </message>
+    <message>
+        <source>Hunt and Kill</source>
+        <translation>Hunt and Kill</translation>
+    </message>
+    <message>
+        <source>Kruskal</source>
+        <translation>Kruskal</translation>
+    </message>
+    <message>
+        <source>Prim</source>
+        <translation>Prim</translation>
+    </message>
+    <message>
+        <source>Recursive Backtracker</source>
+        <translation>Recursive Backtracker</translation>
+    </message>
+    <message>
+        <source>Stack</source>
+        <translation>Stack</translation>
+    </message>
+    <message>
+        <source>Stack 2</source>
+        <translation>Stack 2</translation>
+    </message>
+    <message>
+        <source>Stack 3</source>
+        <translation>Stack 3</translation>
+    </message>
+    <message>
+        <source>Stack 4</source>
+        <translation>Stack 4</translation>
+    </message>
+    <message>
+        <source>Stack 5</source>
+        <translation>Stack 5</translation>
+    </message>
+    <message>
+        <source>Algorithm:</source>
+        <translation>Αλγόριθμος</translation>
+    </message>
+    <message>
+        <source>Targets:</source>
+        <translation>Στόχοι:</translation>
+    </message>
+    <message>
+        <source>Size:</source>
+        <translation>Μέγεθος:</translation>
+    </message>
+</context>
+<context>
+    <name>QLabel</name>
+    <message>
+        <source>Hunt and Kill</source>
+        <translation>Hunt and Kill</translation>
+    </message>
+    <message>
+        <source>Kruskal</source>
+        <translation>Kruskal</translation>
+    </message>
+    <message>
+        <source>Prim</source>
+        <translation>Prim</translation>
+    </message>
+    <message>
+        <source>Recursive Backtracker</source>
+        <translation>Recursive Backtracker</translation>
+    </message>
+    <message>
+        <source>Stack</source>
+        <translation>Stack</translation>
+    </message>
+    <message>
+        <source>Stack 2</source>
+        <translation>Stack 2</translation>
+    </message>
+    <message>
+        <source>Stack 3</source>
+        <translation>Stack 3</translation>
+    </message>
+    <message>
+        <source>Stack 4</source>
+        <translation>Stack 4</translation>
+    </message>
+    <message>
+        <source>Stack 5</source>
+        <translation>Stack 5</translation>
+    </message>
+</context>
+<context>
+    <name>Scores</name>
+    <message>
+        <source>CuteMaze Scores</source>
+        <translation>Αποτελέσματα CuteMaze</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Όνομα</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Αποτέλεσμα</translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>Χρόνος</translation>
+    </message>
+    <message>
+        <source>Steps</source>
+        <translation>Βήματα</translation>
+    </message>
+    <message>
+        <source>Algorithm</source>
+        <translation>Αλγόριθμος</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Μέγεθος</translation>
+    </message>
+    <message>
+        <source>Congratulations!</source>
+        <translation>Συγχαρητήρια!</translation>
+    </message>
+    <message>
+        <source>Your score has made the top ten.
+Please enter your name:</source>
+        <translation>Το σκορ σας είναι μέσα στα 10 πρώτα. Παρακαλώ εισάγετε τ&apos; όνομά σας:</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Ρυθμίσεις</translation>
+    </message>
+    <message>
+        <source>Game</source>
+        <translation>Παιχνίδι</translation>
+    </message>
+    <message>
+        <source>Show where you&apos;ve been</source>
+        <translation>Εμφάνιση διαδρομής</translation>
+    </message>
+    <message>
+        <source>Show number of steps taken</source>
+        <translation>Εμφάνιση αριθμού βημάτων</translation>
+    </message>
+    <message>
+        <source>Show elapsed time</source>
+        <translation>Εμφάνιση παρελθόντος χρόνου</translation>
+    </message>
+    <message>
+        <source>Smooth movement</source>
+        <translation>Ομαλή κίνηση</translation>
+    </message>
+    <message>
+        <source>Controls</source>
+        <translation>Έλεγχοι</translation>
+    </message>
+    <message>
+        <source>Move Up:</source>
+        <translation>Κίνηση Επάνω:</translation>
+    </message>
+    <message>
+        <source>Move Down:</source>
+        <translation>Κίνηση Κάτω:</translation>
+    </message>
+    <message>
+        <source>Move Left:</source>
+        <translation>Κίνηση Αριστερά:</translation>
+    </message>
+    <message>
+        <source>Move Right:</source>
+        <translation>Κίνηση Δεξιά:</translation>
+    </message>
+    <message>
+        <source>Toggle Flag:</source>
+        <translation>Εναλλαγή Σημαίας:</translation>
+    </message>
+    <message>
+        <source>Themes</source>
+        <translation>Θέματα</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Πρόσθεση</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Αφαίρεση</translation>
+    </message>
+    <message>
+        <source>Select Theme File</source>
+        <translation>Επιλογή Αρχείου Θέματος</translation>
+    </message>
+    <message>
+        <source>Sorry</source>
+        <translation>Συγνώμη</translation>
+    </message>
+    <message>
+        <source>Unable to create data folder.</source>
+        <translation>Αδυναμία δημιουργίας φακέλου δεδομένων.</translation>
+    </message>
+    <message>
+        <source>Unable to copy theme file.</source>
+        <translation>Αδυναμία αντιγραφής αρχείου θέματος.</translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation>Ερώτηση</translation>
+    </message>
+    <message>
+        <source>Remove the selected theme?</source>
+        <translation>Αφαίρεση επιλεγμένου θέματος;</translation>
+    </message>
+    <message>
+        <source>Unable to remove the selected theme.</source>
+        <translation>Αδυναμία αφαίρεσης επιλεγμένου θέματος.</translation>
+    </message>
+    <message>
+        <source>Show Hint:</source>
+        <translation>Εμφάνιση υπόδειξης:</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <source>CuteMaze</source>
+        <translation>CuteMaze</translation>
+    </message>
+    <message>
+        <source>P</source>
+        <translation>P</translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation>Εμφάνιση</translation>
+    </message>
+    <message>
+        <source>Ctrl++</source>
+        <translation>Ctrl++</translation>
+    </message>
+    <message>
+        <source>Ctrl+-</source>
+        <translation>Ctrl+-</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Σχετικά</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>Σχετικά &amp;Qt</translation>
+    </message>
+    <message>
+        <source>&amp;Game</source>
+        <translation>&amp;Παιχνίδι</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Νέο</translation>
+    </message>
+    <message>
+        <source>&amp;Pause</source>
+        <translation>&amp;Παύση</translation>
+    </message>
+    <message>
+        <source>&amp;Hint</source>
+        <translation>&amp;Υπόδειξη</translation>
+    </message>
+    <message>
+        <source>High &amp;Scores</source>
+        <translation>Υψηλές &amp;Βαθμολογίες</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Έξοδος</translation>
+    </message>
+    <message>
+        <source>Zoom &amp;In</source>
+        <translation>Μεγέθυνση</translation>
+    </message>
+    <message>
+        <source>Zoom &amp;Out</source>
+        <translation>Σμίκρυνση</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Ρυθμίσεις</translation>
+    </message>
+    <message>
+        <source>Application &amp;Language...</source>
+        <translation>Γλώσσα &amp;Εφαρμογής...</translation>
+    </message>
+    <message>
+        <source>&amp;Preferences...</source>
+        <translation>&amp;Προτιμήσεις...</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Βοήθεια</translation>
+    </message>
+    <message>
+        <source>&amp;About</source>
+        <translation>&amp;Σχετικά</translation>
+    </message>
+    <message>
+        <source>A top-down maze game</source>
+        <translation>Ένα κατακόρυφου προσανατολισμού παιχνίδι λαβυρίνθου</translation>
+    </message>
+    <message>
+        <source>Copyright &amp;copy; 2007-%1 Graeme Gott</source>
+        <translation>Πνευματική ιδιοκτησία; 2007-%1 Graeme Gott</translation>
+    </message>
+    <message>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Κυκλοφορία υποκείμενη σε &lt;a href=%1&gt;GPL 3&lt;/a&gt; άδεια</translation>
+    </message>
+    <message>
+        <source>Icons are from the &lt;a href=%1&gt;Oxygen&lt;/a&gt; theme</source>
+        <translation>Οι εικόνες είναι από &lt;a href=%1&gt;Oxygen&lt;/a&gt; θέμα</translation>
+    </message>
+    <message>
+        <source>Used under the &lt;a href=%1&gt;LGPL 3&lt;/a&gt; license</source>
+        <translation>Χρήση υπό &lt;a href=%1&gt;LGPL 3&lt;/a&gt; άδεια</translation>
+    </message>
+</context>
+</TS>
