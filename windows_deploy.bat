c19a614f06a9b7e53c74e14562cc7021e9cbe77f
Graeme Gott <graeme@gottcode.org>
2020-12-08 05:02:06 -0500

    Update windows deployment script.

--- windows_deploy.bat
+++ windows_deploy.bat
@@ -1,13 +1,15 @@
-@ECHO ON>windows\dirs.nsh
-@ECHO ON>windows\files.nsh
+@ECHO ON>..\cutemaze\windows\dirs.nsh
+@ECHO ON>..\cutemaze\windows\files.nsh
 @ECHO OFF
 
+SET SRCDIR=..\cutemaze
 SET APP=CuteMaze
 SET VERSION=1.2.6
 
 ECHO Copying executable
-MKDIR %APP%
-COPY release\%APP%.exe %APP% >nul
+MKDIR %SRCDIR%\%APP%
+COPY release\%APP%.exe %SRCDIR%\%APP% >nul
+CD %SRCDIR%
 
 ECHO Copying translations
 SET TRANSLATIONS=%APP%\translations
@@ -15,7 +17,7 @@ MKDIR %TRANSLATIONS%
 COPY translations\*.qm %TRANSLATIONS% >nul
 
 ECHO Copying Qt
-%QTDIR%\bin\windeployqt.exe --verbose 0 --no-angle --no-opengl-sw %APP%\%APP%.exe
+%QTDIR%\bin\windeployqt.exe --verbose 0 --no-opengl-sw --no-system-d3d-compiler %APP%\%APP%.exe
 RMDIR /S /Q %APP%\iconengines
 RMDIR /S /Q %APP%\imageformats
 
